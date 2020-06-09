e85a7eb9f972ac8b5a56888e9cd94743914f7c5c
gym_global <106777_global@qq.com>
2020-06-09 22:08:34 +0800

    第一次创建分支

--- ch04-maven-git/src/main/java/com/yun/git/HelloGit.java
+++ ch04-maven-git/src/main/java/com/yun/git/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello yuxiao");
+        System.out.println("创建test1分支");
     }
 
 }
